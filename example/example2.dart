2591f5b039d14ab72bef03acf17a02d942b24801
Roberto Tassi <roberto.tassi@fromlabs.org>
2016-02-01 09:42:53 +0100

    Examples.

---
+++ example/example2.dart
@@ -0,0 +1,72 @@
+library example;
+
+import "dart:async";
+
+import "package:logging/logging.dart";
+
+import "package:dartregistry/dart_registry.dart";
+
+final Logger _libraryLogger = new Logger("example");
+
+const Scope REQUEST_SCOPE = const Scope("REQUEST");
+
+@injectable
+class ExampleModule extends RegistryModule {
+  @override
+  void configure() {
+    bindClass(GlobalService, Scope.ISOLATE);
+
+    bindClass(RequestService, REQUEST_SCOPE);
+  }
+}
+
+@injectable
+class GlobalService {
+  @inject
+  RequestService service;
+
+  @Inject(RequestService)
+  Provider<RequestService> serviceProvider;
+
+  test() {
+    service.test();
+  }
+
+  testProvider() {
+    serviceProvider.get().test();
+  }
+}
+
+@injectable
+class RequestService {
+  @inject
+  GlobalService service;
+
+  test() {
+    print("request");
+  }
+}
+
+main() async {
+  Logger.root.level = Level.INFO;
+  Logger.root.onRecord.listen((record) {
+    print("*****");
+    print(record);
+    print(record.error);
+    print(record.stackTrace);
+  });
+
+  try {
+    Registry.load(new ExampleModule());
+
+    await Registry.runInIsolateScope(() async {
+      await Registry.runInScope(REQUEST_SCOPE, () {
+        Registry.lookupObject(GlobalService).test();
+
+        Registry.lookupObject(GlobalService).testProvider();
+      });
+    });
+  } finally {
+    Registry.unload();
+  }
+}
---
+++ example/example3.dart
@@ -0,0 +1,75 @@
+library example;
+
+import "dart:async";
+
+import "package:logging/logging.dart";
+
+import "package:dartregistry/dart_registry.dart";
+
+final Logger _libraryLogger = new Logger("example");
+
+const Scope REQUEST_SCOPE = const Scope("REQUEST");
+
+@injectable
+class ExampleModule extends RegistryModule {
+  @override
+  void configure() {
+    bindClass(Service, Scope.ISOLATE);
+
+    bindProvideFunction(
+        Connection,
+        REQUEST_SCOPE,
+        () => new Future.delayed(new Duration(seconds: 1))
+            .then((_) => new ConnectionImpl()));
+  }
+}
+
+@injectable
+class Service {
+  @Inject(Connection)
+  Provider<Future<Connection>> connectionProvider;
+
+  Future run() async {
+    var connection = await connectionProvider.get();
+    await connection.query();
+  }
+}
+
+@injectable
+abstract class Connection {
+  Future query();
+}
+
+@injectable
+class ConnectionImpl implements Connection {
+  Future query() async {
+    print("Query execution...");
+    await new Future.delayed(new Duration(seconds: 1));
+    print("Query executed!");
+  }
+}
+
+main() async {
+  Logger.root.level = Level.INFO;
+  Logger.root.onRecord.listen((record) {
+    print("*****");
+    print(record);
+    print(record.error);
+    print(record.stackTrace);
+  });
+
+  try {
+    Registry.load(new ExampleModule());
+
+    await Registry.runInIsolateScope(() async {
+      print("Request begin");
+      await Registry.runInScope(REQUEST_SCOPE, () async {
+        var service = Registry.lookupObject(Service) as Service;
+        await service.run();
+      });
+      print("Request end");
+    });
+  } finally {
+    Registry.unload();
+  }
+}
